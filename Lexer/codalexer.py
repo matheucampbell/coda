017fbea5bdd493d0ff9d28ecd39c8741706ad76f
matheucampell <matheu.campbell.us@outlook.com>
2024-10-16 18:37:42 -0400

    comments supported

--- Lexer/codalexer.py
+++ Lexer/codalexer.py
@@ -11,14 +11,16 @@ class Token:
 class Regex:
     '''Simplified regular expression class'''
     def __init__(self, token_class, text_re, stage_dict, 
-                 optionals=[], repeats=[]):
+                 optionals=[], repeats=[], inverts=[]):
         self.textexp = text_re
         self.tclass = token_class
         self.stages = stage_dict
         self.opts = optionals
-        self.minfin = max(set(stage_dict.keys()) - set(optionals))  # Minimum final stage
+        self.reps = repeats
+        self.invs = inverts
+        self.final = max(set(stage_dict.keys()) - set(optionals))  # Final stage
         self.max_stage = max(stage_dict.keys())
-    
+
     def search(self, input):
         '''Search for a match in given input'''
         return self.match(input)
@@ -30,25 +32,35 @@ class Regex:
         while i < len(input) and stage <= self.max_stage:
             c = input[i]
             valid_chars = self.stages[stage]
-            if stage not in self.opts and c in valid_chars:
+
+            if stage not in self.invs:
+                valid = c in valid_chars
+            else:
+                valid = c not in valid_chars
+            
+            if stage not in self.opts and stage not in self.reps and valid:
                 # print(f"Stage {stage} match: {c}")
                 i += 1
                 stage += 1
             elif stage in self.opts:
-                if c in valid_chars:
-                    # print(f"Stage {stage} match: {c}")
+                if valid:
+                    # print(f"Stage {stage} (optional) match: {c}")
                     i += 1
-                # else:
-                    # print(f"Stage {stage} (optional) skipped.")
                 stage += 1
+            elif stage in self.reps:
+                if valid:
+                    # print(f"Stage {stage} (arbitrarily repeated) match: {c}")
+                    i += 1
+                else:
+                    stage += 1
             else:  # Stage not matched and not optional
                 # print(f"Stage {stage} for {self.tclass} not matched. Current char: {c}")
-                # print(c, self.opts, self.stages[stage])
+                # print(c, self.opts, stage)
                 return None
         
-        # Check that minimum final stage was reached
-        if stage - 1 >= self.minfin:
-            return Token(self.tclass, input[:i])
+        # Check that final stage was reached
+        if stage - 1 >= self.final:
+            return Token(self.tclass, input[:i].strip('\n'))
         print(f"Minimum final stage not reached. Current stage: {stage}")
         return None
     
@@ -72,20 +84,23 @@ class Lexer:
         '''Tokenizes the entire input string.'''
         with open(self.input_path, 'r') as infile:
             self.input = infile.read()
-            self.input = self.input.replace(" ", "")
-            self.input = "".join(self.input.splitlines())
+            # self.input = self.input.replace(" ", "")
+            # self.input = "".join(self.input.splitlines())
             # print(self.input)
 
         while self.position < len(self.input):
+            self.skip_whitespace()
             self.exec_dfa()
 
     def get_tokens(self):
         '''Returns list of tokens after lexing.'''
         return self.tokens
 
-    def advance(self):
-        '''Advances to the next character in the input stream.'''
-        self.position += 1
+    def skip_whitespace(self):
+        '''Advances to the next non-newline character in input stream.'''
+        while self.position < len(self.input) and \
+              (self.input[self.position] == '\n' or self.input[self.position] == ' '):
+            self.position += 1
 
     def exec_dfa(self):
         '''
@@ -97,11 +112,11 @@ class Lexer:
         for regex in self.token_bank:
             matches.append(regex.search(self.input[self.position:]))
         
-        # Maximal munch
         matches = [m for m in matches if m]
         if not matches:
-            raise LexerException(f"No valid token at position {self.position}: '{self.input[self.position:self.position+10]}...'")
+            raise LexerException(f"No valid token at position {self.position}: '{self.input[self.position:min(self.position+10, len(self.input)-1)]}...'")
         
+        # Maximal munch
         matchlens = [len(m.text) for m in matches]
         zipped = zip(matches, matchlens)
         match = sorted(zipped, key=lambda x: x[1])[-1][0]
