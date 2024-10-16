4f9e2ce670f082b0e9b8757fa414ac100b780995
matheucampell <matheu.campbell.us@outlook.com>
2024-10-16 09:55:23 -0400

    more templates

--- Lexer/codalexer.py
+++ Lexer/codalexer.py
@@ -7,11 +7,21 @@ class Token:
         return f"<{self.name}, VALUE={self.value}>"
 
 
+class SimpleRegex:
+    '''Simplified regular expression class'''
+    def __init__(self, stage_list):
+        pass
+
+
 class Lexer:
     def __init__(self, input_path):
         self.input = self.read_input(input_path)
         self.position = 0
         self.tokens = []
+
+    def register_token(self, name, regex):
+        '''Adds a recognized token type'''
+        pass
 
     def tokenize(self):
         '''Tokenizes input string'''
@@ -24,6 +34,19 @@ class Lexer:
     def advance(self):
         '''Advances to next character in text stream'''
         pass
+
+    def exec_dfa(self):
+        '''
+        Runs DFA starting at current token and appends new token to list.
+        Raises exception on lexical error.
+        '''
+        char = self.input[self.pos]
+
+        # Single-character tokens
+        if char in self.singles:
+            pass
+
+        # Multi-character processing
 
 
 class LexerException(Exception):
