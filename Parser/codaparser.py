3acb47b0cf1451cc2b9cfbdc1dc2b249be4c7653
matheucampell <matheu.campbell.us@outlook.com>
2024-11-12 14:35:40 -0500

    debugging statements

--- Parser/codaparser.py
+++ Parser/codaparser.py
@@ -18,16 +18,20 @@ class Parser:
             self.advance()
 
     def advance(self):
-        cur_tok = self.lookahead  # Token object
-        cur_sym = self.derived[self.pos]  # Token or string
+        print(self.derived)
+        cur_tok = self.input[self.pos]  # Token object
+        cur_sym = self.derived[0]  # Token or string
         if not isinstance(cur_sym, Token):
+            print(f"Expanding '{cur_sym}'; Looking at '{cur_tok}'")
             expanded = self.ptab.get_production(cur_sym, cur_tok)
+            print(f"Expanded to {expanded}")
             self.derived = expanded + self.derived[1:]  # Should be list of tokens and strings
         else:  # Match token
+            print(f"Attempting to match {str(cur_tok)}")
             if cur_tok.equals(cur_sym):
-                print(f"Matched {cur_tok}")
                 self.pos += 1
                 self.derived = self.derived[1:]
+                print("Matched successfully.")
             else:  # Error
                 pass
 
@@ -45,10 +49,13 @@ class ParseTable:  # LL(1) Parsing
         Returns production to take given current nonterminal and lookahead.
         If entry is not found, return None. Caller handles error.
         '''
-        print(f"Fetching: {nonterm, lookahead}")
+        # print(f"Fetching: {nonterm, lookahead}")
         for e in self.entries.keys():
             if e[0] == nonterm and lookahead.equals(e[1]):
-                return self.entries[(nonterm, lookahead)]
+                # print("Found next production")
+                return self.entries[e]
+        print("Failed to find matching production")
+        return None
 
 
 class Production:
