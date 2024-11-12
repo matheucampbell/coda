ae649022188b16ae1808bd2d0719598094e133cd
matheucampell <matheu.campbell.us@outlook.com>
2024-11-12 11:39:58 -0500

    matching logic started

--- Parser/codaparser.py
+++ Parser/codaparser.py
@@ -7,14 +7,24 @@ class Parser:
     def __init__(self, input: List[Token], parse_table):
         self.pos = 0
         self.lookahead = input[self.pos]
+        self.derived = []  # Derived string
         self.ptab = parse_table
-        self.derived = []  # List of Token or Production
+
+    def advance(self):
+        cur_tok = self.lookahead
+        cur_sym = self.derived[self.pos]
+        if cur_sym.isinstance(Production):  # Expand production
+            self.derived.insert(0, self.ptab(cur_sym, cur_tok))
+        else:  # Match token
+            if cur_tok.equals(cur_sym):  # Need to write equals() method for Token class to deal with keywords
+                self.pos += 1
+            else:  # Error
+                pass
 
 
 class ParseTable:  # LL(1) Parsing
     def __init__(self, productions):
         self.entries = {}  # {(nonterminal, Token) : Production, ...}
-        self.prods = productions
 
     def register_entry(self):
         '''Add a cell to the table.'''
