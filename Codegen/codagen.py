0ea3dc71844291f66d54017b8f3d093056155f30
matheucampell <matheu.campbell.us@outlook.com>
2024-12-09 18:36:34 -0500

    skeletons for parsing

--- Codegen/codagen.py
+++ Codegen/codagen.py
@@ -4,17 +4,53 @@ class CodaGenerator:
     def __init__(self, ast_root, outname='output'):
         self.root = ast_root
         self.outname = outname
+        self.typstack = [1]
+        self.repstack = [1]
+        self.grpstack = [1]
+        self.keysig = None
+        self.timesig = None
+        self.tempo = None
     
     def generate(self):
-        tempo, tokens, durations = self.get_sequences()
-        gen = MidiGenerator(tempo, tokens, durations)
+        tokens, durations = self.get_sequences()
+        gen = MidiGenerator(self.tempo, tokens, durations)
         gen.generate(self.outname)
     
     def get_sequences(self):
         '''Parses AST to generate input for MidiGenerator'''
         # 1. Pass 1: Get global identifiers
 
-        # 2. Traverse breadth-first, hierarchically applying modifiers to blocks and notes
+        # 2. Track current modifiers on relevant stacks and recursively process note blocks
+        # Stacks initialized to typ=1, rep=1, grp=1
     
-    def parse_block(self, tmod=None, rmod=None, gmod=None):
-        ''''''
+    def get_globals(self, node):
+        '''Extract global identifiers. Treat the rest of the program as one note block.'''
+        for child in node.children:
+            # Advance to key signature
+            pass
+            # Advence to time signature
+
+            # Advance to tempo
+
+    def parse_block(self, node):
+        '''Parse a note block'''
+        # typ not None -> All notes have specified duration
+        # rep not None -> Entire block repeated as specified
+        # grp not None -> Group first n notes in block
+        note_seq = []
+        dur_seq = []
+
+        for child in node.children:
+            while child.value != 'NOTE BLOCK':
+                if child.value == 'typ':
+                    pass
+                elif child.value == 'rep':
+                    pass
+                elif child.value == 'grp':
+                    pass
+                elif child.value == 'NOTE' or child.value == 'CHORD':
+                    pass
+            else:
+                ns, ds = self.parse_block(node)
+                note_seq += ns
+                dur_seq += ds
