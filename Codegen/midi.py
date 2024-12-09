92bc3ee42f4bff5d0f4ed74277e7e43655ee7ba0
matheucampbell <matheu.campbell.us@outlook.com>
2024-12-08 20:17:44 -0500

    early implementation

--- Codegen/midi.py
+++ Codegen/midi.py
@@ -2,6 +2,29 @@ import struct
 
 # Format Specification: https://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BM0_
 
+def encode_vlq(value):
+    '''Encodes an integer into a Variable-Length Quantity (VLQ)'''
+    buffer = value & 0x7F
+    while (value := value >> 7):
+        buffer <<= 8
+        buffer |= (value & 0x7F) | 0x80
+    vlq = bytearray()
+    while True:
+        vlq.append(buffer & 0xFF)
+        if buffer & 0x80:
+            buffer >>= 8
+        else:
+            break
+    return bytes(vlq)
+
+def convert_tempo(tempo):
+    '''Converts tempo in bpm to ticks per quarter-note'''
+    tpq = 480  # Typical, but varies
+    microseconds_per_beat = 60000000 / tempo  # microseconds per beat (1 minute / BPM)
+    ticks_per_quarter_note = microseconds_per_beat / (60000000 / tpq)  # microseconds_per_beat / microseconds_per_tick
+
+    return int(ticks_per_quarter_note)
+
 
 class HeaderChunk:
     '''MIDI header chunk'''
@@ -9,15 +32,7 @@ class HeaderChunk:
     def __init__(self, tempo):
         self.fmt = 0
         self.ntracks = 1
-        self.div = self._convert_tempo(tempo)
-    
-    def _convert_tempo(self, tempo):
-        '''Converts tempo in bpm to ticks per quarter-note'''
-        tpq = 480  # Typical, but varies
-        microseconds_per_beat = 60000000 / tempo  # microseconds per beat (1 minute / BPM)
-        ticks_per_quarter_note = microseconds_per_beat / (60000000 / tpq)  # microseconds_per_beat / microseconds_per_tick
-
-        return int(ticks_per_quarter_note)
+        self.div = convert_tempo(tempo)
 
     def bytes(self):
         '''Returns the 14-bit binary equivalent of header'''
@@ -31,15 +46,46 @@ class HeaderChunk:
 
 class TrackChunk:
     '''MIDI Track Chunk'''
-    def __init__(self):
+    ID = b"MTrk"
+    def __init__(self, tpq=480):
         self.notes = b""
+        self.tpq = tpq
 
     def add_note(self, note, dur):
+        '''Adds a note (in string format) for a given duration (in beats)'''
+        note_number = NOTE_MAP[note]
+        ticks_duration = int(dur * self.tpq)  # Convert beats to ticks
+        
+        # Encode delta time for the note-on event
+        self.notes += struct.pack(">B", 0)
+        self.notes += struct.pack(">BBB", 0x90, note_number, 64)  # Note-on (velocity 64)
+        
+        # Encode delta time for the note-off event
+        self.notes += encode_vlq(ticks_duration)  # Delta time (variable length)
+        self.notes += struct.pack(">BB", 0x80, note_number)  # Note-off (velocity 0)
+
+    def add_chord(self, notes, dur):
         pass
     
     def bytes(self):
         header = self.ID + struct.pack(">I", len(self.notes))
         return header + self.notes
+
+
+class Generator:
+    def __init__(self, tempo, note_seq, dur_seq):
+        self.header = HeaderChunk(tempo)
+        self.track = TrackChunk()
+        self.note_seq = note_seq
+        self.dur_seq = dur_seq
+    
+    def generate(self, fname):
+        for num, note in enumerate(self.note_seq):
+            self.track.add_note(note, self.dur_seq[num])
+
+        with open(f'{fname}.mid', "wb") as midi:
+            midi.write(self.header.bytes())
+            midi.write(self.track.bytes())
 
 
 # Note Map - Maps text notes from C1 to C5 (sharps specified as ex. C#1 and flats as ex. Cb1)
@@ -56,5 +102,14 @@ NOTE_MAP = {
     "G#4": 56, "Ab4": 56, "A4": 57, "A#4": 58, "Bb4": 58, "B4": 59, "C5": 60
 }
 
-header = HeaderChunk(160)
-header.bytes()
+# Chord Map - Maps chords to lists of notes from C1 to C5 (supports major and minor | Ex. C-sharp major = C#+ and C-flat minor = ex. Cb-)
+CHORD_MAP = {
+
+}
+
+nseq = ['F4', 'A#4', 'F4']
+dseq = [2, 2, 2]
+tempo = 120
+
+gen = Generator(tempo, nseq, dseq)
+gen.generate('demo')
