fd3e9135f22c709b47613ab7c8bb6f651424ba4d
matheucampell <matheu.campbell.us@outlook.com>
2024-12-09 17:09:04 -0500

    chord support

--- Codegen/midi.py
+++ Codegen/midi.py
@@ -46,7 +46,8 @@ class TrackChunk:
             self.add_note(item, dur)
 
     def add_note(self, note, dur):
-        '''Adds a note (in string format) for a given duration (in beats)'''
+        '''Adds a note for a given duration (in beats)'''
+        note = note.text
         ticks_duration = int(dur*self.tpq)  # Convert beats to ticks
 
         if note == '_':  # On rest, increment current delay and continue
@@ -65,7 +66,21 @@ class TrackChunk:
     
     def add_chord(self, chord, dur):
         '''Adds a chord (in string format) for a given duration (in beats)'''
-        pass
+        chord = chord.text  # Ex: C+5*
+        base, oct = chord[:-2], chord[-2]
+        notes = CHORD_MAP[base]  # ["C", "E", "G"]
+
+        self.events += encode_vlq(self.rest_ticks)
+        for note in notes[1:]:  # Add all notes at the same time
+            note_num = NOTE_MAP[note+oct]
+            self.events += struct.pack(">BBB", 0x90, note_num, 64)
+        self.rest_ticks = 0
+
+        self.events += encode_vlq(int(dur*self.tpq))  # Delta time
+        for note in notes:  # Stop all notes at once
+            note_num = NOTE_MAP[note+oct]
+            self.events += struct.pack(">BBB", 0x80, note_num, 0)  # Note-off (velocity 0)
+
 
     def add_meta_event(self, meta_type, data):
         '''Adds a meta event'''
@@ -95,7 +110,7 @@ class Generator:
         self.track.add_meta_event(0x51, tempo_data)
 
         for num, note in enumerate(self.note_seq):
-            self.track.add_note(note, self.dur_seq[num])
+            self.track.add(note, self.dur_seq[num])
 
         self.track.end_track()
 
@@ -104,18 +119,36 @@ class Generator:
             midi.write(self.track.bytes())
 
 
-# Note Map - Maps text notes from C1 to C5 (sharps specified as ex. C#1 and flats as ex. Cb1)
+# Note Map - Maps text notes from C1 to B7 (sharps specified as ex. C#1 and flats as ex. Cb1)
 NOTE_MAP = {
+    # Octave 1
     "C1": 12, "C#1": 13, "Db1": 13, "D1": 14, "D#1": 15, "Eb1": 15,
     "E1": 16, "F1": 17, "F#1": 18, "Gb1": 18, "G1": 19, "G#1": 20, "Ab1": 20,
-    "A1": 21, "A#1": 22, "Bb1": 22, "B1": 23, "C2": 24, "C#2": 25, "Db2": 25,
-    "D2": 26, "D#2": 27, "Eb2": 27, "E2": 28, "F2": 29, "F#2": 30, "Gb2": 30,
-    "G2": 31, "G#2": 32, "Ab2": 32, "A2": 33, "A#2": 34, "Bb2": 34, "B2": 35,
-    "C3": 36, "C#3": 37, "Db3": 37, "D3": 38, "D#3": 39, "Eb3": 39, "E3": 40,
-    "F3": 41, "F#3": 42, "Gb3": 42, "G3": 43, "G#3": 44, "Ab3": 44, "A3": 45,
-    "A#3": 46, "Bb3": 46, "B3": 47, "C4": 48, "C#4": 49, "Db4": 49, "D4": 50,
-    "D#4": 51, "Eb4": 51, "E4": 52, "F4": 53, "F#4": 54, "Gb4": 54, "G4": 55,
-    "G#4": 56, "Ab4": 56, "A4": 57, "A#4": 58, "Bb4": 58, "B4": 59, "C5": 60
+    "A1": 21, "A#1": 22, "Bb1": 22, "B1": 23,
+    # Octave 2
+    "C2": 24, "C#2": 25, "Db2": 25, "D2": 26, "D#2": 27, "Eb2": 27,
+    "E2": 28, "F2": 29, "F#2": 30, "Gb2": 30, "G2": 31, "G#2": 32, "Ab2": 32,
+    "A2": 33, "A#2": 34, "Bb2": 34, "B2": 35,
+    # Octave 3
+    "C3": 36, "C#3": 37, "Db3": 37, "D3": 38, "D#3": 39, "Eb3": 39,
+    "E3": 40, "F3": 41, "F#3": 42, "Gb3": 42, "G3": 43, "G#3": 44, "Ab3": 44,
+    "A3": 45, "A#3": 46, "Bb3": 46, "B3": 47,
+    # Octave 4
+    "C4": 48, "C#4": 49, "Db4": 49, "D4": 50, "D#4": 51, "Eb4": 51,
+    "E4": 52, "F4": 53, "F#4": 54, "Gb4": 54, "G4": 55, "G#4": 56, "Ab4": 56,
+    "A4": 57, "A#4": 58, "Bb4": 58, "B4": 59,
+    # Octave 5
+    "C5": 60, "C#5": 61, "Db5": 61, "D5": 62, "D#5": 63, "Eb5": 63,
+    "E5": 64, "F5": 65, "F#5": 66, "Gb5": 66, "G5": 67, "G#5": 68, "Ab5": 68,
+    "A5": 69, "A#5": 70, "Bb5": 70, "B5": 71,
+    # Octave 6
+    "C6": 72, "C#6": 73, "Db6": 73, "D6": 74, "D#6": 75, "Eb6": 75,
+    "E6": 76, "F6": 77, "F#6": 78, "Gb6": 78, "G6": 79, "G#6": 80, "Ab6": 80,
+    "A6": 81, "A#6": 82, "Bb6": 82, "B6": 83,
+    # Octave 7
+    "C7": 84, "C#7": 85, "Db7": 85, "D7": 86, "D#7": 87, "Eb7": 87,
+    "E7": 88, "F7": 89, "F#7": 90, "Gb7": 90, "G7": 91, "G#7": 92, "Ab7": 92,
+    "A7": 93, "A#7": 94, "Bb7": 94, "B7": 95
 }
 
 # Chord Map - Maps chords to lists of notes from C1 to C5 (supports major and minor | Ex. C-sharp major = C#+ and C-flat minor = ex. Cb-)
@@ -146,8 +179,10 @@ CHORD_MAP = {
     "B-": ["B", "D", "F#"]
 }
 
-nseq = ['F4', 'G#4', '_', 'F4']
-dseq = [1, 1, 1, 1]
+from parse import Token
+nseq = [Token('NOTE', 'F4'), Token('CHORD', 'A#+5*'), Token('NOTE', 'F4'), Token('NOTE', 'F4')]
+nseq = [Token('CHORD', 'A#+5*')]
+dseq = [1]
 tempo = 120
 
 gen = Generator(tempo, nseq, dseq)
