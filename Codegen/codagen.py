802bc136e2bb9439ec32ec08e02ad770a3e3f1ce
matheucampell <matheu.campbell.us@outlook.com>
2024-12-09 19:52:00 -0500

    partial parsing

--- Codegen/codagen.py
+++ Codegen/codagen.py
@@ -1,12 +1,13 @@
+from codaparser import TokenNodeAST, ProductionNodeAST
 from midi import MidiGenerator
 
 class CodaGenerator:
     def __init__(self, ast_root, outname='output'):
         self.root = ast_root
         self.outname = outname
-        self.typstack = [1]
-        self.repstack = [1]
-        self.grpstack = [1]
+        self.typstack = []
+        self.repstack = []
+        self.grpstack = []
         self.keysig = None
         self.timesig = None
         self.tempo = None
@@ -20,10 +21,8 @@ class CodaGenerator:
         '''Parses AST to generate input for MidiGenerator'''
         # 1. Step 1: Get global modifiers
         next_node = self.get_globals()
+        return self.parse_block(next_node, 1, 1, 1)
 
-        # 2. Track current modifiers on relevant stacks and recursively process note blocks
-        # Stacks initialized to typ=1, rep=1, grp=1
-    
     def get_globals(self):
         '''Extract global identifiers. Treat the rest of the program as one note block.'''
         # Can hardcode locations of global identifiers
@@ -35,25 +34,51 @@ class CodaGenerator:
 
         return self.root.children[7]
 
-    def parse_block(self, node):
+    def parse_block(self, node, typ, rep, grp):
         '''Parse a note block'''
         # typ[n] -> All notes have specified duration
         # rep[n] -> Entire block repeated as specified
         # grp[n] -> Group first n notes in block
-        note_seq = []
-        dur_seq = []
 
+        # Update stacks with current level's modifiers
+        self.typstack.append(typ)
+        self.repstack.append(rep)
+        self.grpstack.append(grp)
+
+        # Initialize next level's mods (overwritten if found)
+        next_typ = self.typstack[-1]  # typ modifier inherited by enclosed blocks
+        next_rep = 1  # rep modifier NOT inherited by enclosed blocks
+        next_grp = 1  # grp modifier NOT inherited by enclosed blocks
+
+        note_seq, dur_seq = [], []
+
+        print("NODE: ", node, node.children)
         for child in node.children:
-            while child.value != 'NOTE BLOCK':
-                if child.value == 'typ':
-                    pass
-                elif child.value == 'rep':
-                    pass
-                elif child.value == 'grp':
-                    pass
-                elif child.value == 'NOTE' or child.value == 'CHORD':
-                    pass
+            if isinstance(child, TokenNodeAST):
+                if child.tok.token_class != 'CHORD' and child.tok.token_class != 'NOTE':
+                    continue
+            
+            if child.value != 'NOTE BLOCK':
+                if child.value == 'DURATION MODIFIER':
+                    next_typ = child.children[2]
+                elif child.value == 'REPETITION MODIFIER':
+                    next_rep = child.children[2]
+                elif child.value == 'GROUPING MODIFIER':
+                    next_grp = child.children[2]
+                elif child.value == 'NOTE/CHORD/REST':
+                    # Use current duration modifier to add values to notes
+                    note_seq.append(child.children[0])
+                    dur_seq.append(self.typstack[-1])
             else:
-                ns, ds = self.parse_block(node)
+                ns, ds = self.parse_block(node, next_typ, next_rep, next_grp)
                 note_seq += ns
                 dur_seq += ds
+        
+        note_seq = note_seq * self.repstack[-1]
+        dur_seq = dur_seq * self.repstack[-1]
+
+        self.typstack.pop()
+        self.repstack.pop()
+        self.grpstack.pop()
+
+        return note_seq, dur_seq
